5605aae72371ea77ae403b5fc2b15dee5e53a1f4
Meet Rakholiya <meet@investifyd.com>
2023-09-01 11:55:25 +0530

    nexts-blog app

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,9 @@ import Layout, { siteTitle } from "../components/Layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
 
+import Link from "next/link";
+import Date from "../components/Date";
+
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
   return {
@@ -31,11 +34,11 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
